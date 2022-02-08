84c4cf15e21f091fe24ba532a1e7d4c963ac964d
mikewise5 <mike.wise@kellogg.northwestern.edu>
2022-02-08 17:27:07 +0000

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  
+  ##this is a list of all the resources our application covers. first resource, for example is about tacos (web apps are collections of resources). then do rails server
+  resources :tacos
+  resources :dice
+  resources :cards
+  resources :companies
+  resources :contacts
 end
